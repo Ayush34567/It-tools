3ad2c245a9c62eaae707f703ccb25a496babe355
Ayush34567 <122000204+Ayush34567@users.noreply.github.com>
2023-01-05 11:13:46 +0530

    Add files via upload

---
+++ fy79.py
@@ -0,0 +1,19 @@
+class rectangle:
+    def calculateArea(self):
+        print("enter length:")
+        self.l=float (input())
+        print("enter bredth:")
+        self.b=float(input())
+        area=self.1*self.b
+        print("area of rectangle is =%f"%(area))
+
+    def calculateprimeter(self):
+        perimeter=2*(self.l+self.b)
+        print("permeter of rectangle is =%f"%(perimeter))
+
+
+c=rectangle()
+x=c.Calculaterarear()
+y=c.CalcuatePerimeter()
+
+        
